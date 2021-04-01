267e90990fef5c93d7fd8d5f3ae4dc037592df94
Awoyomi Mayowa <mayowaawoyomi@gmail.com>
2021-04-01 15:19:49 +0100

    with firebase

--- src/feed/Feed.js
+++ src/feed/Feed.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import db from '../firebase'
 import './Feed.css'
 import Post from './Post'
 import TweetBox from './TweetBox'
 
 function Feed() {
+
+    const [post, setPost] = useState([])
+
+
+    useEffect(() => {
+        db.collection('posts').onSnapshot(snapshot => {
+            console.log(snapshot)
+            console.log(snapshot.docs)
+            setPost(snapshot.docs.map(doc => {
+                console.log(doc)
+                console.log(doc.data())
+                return doc.data()
+            }))
+
+
+        })
+    }, [])
     return (
         <div className='feed'>
 
@@ -12,15 +30,20 @@ function Feed() {
             </div>
 
             <TweetBox />
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
+
+            {
+                post.map(doc => (
+                    <Post
+                    displayName={doc.displayName}
+                    avatar={doc.avatar}
+                    image={doc.image} 
+                    text={doc.text}
+                    handle={doc.handle}
+                    verified={doc.verified}/>
+
+                ))
+
+            }
 
         </div>
     )
--- src/feed/TweetBox.js
+++ src/feed/TweetBox.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import db from '../firebase'
 import { Avatar, Button } from '@material-ui/core'
 import ImageIcon from '@material-ui/icons/Image'
 import GifIcon from '@material-ui/icons/Gif'
@@ -10,21 +11,46 @@ import avatar from '../images/bus.jpg'
 import './Tweetbox.css'
 
 function TweetBox() {
+    const [tweet, setTweet] = useState('')
+    const [tweetImage, setTweetImage] = useState('')
+
+    console.log(tweet)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        db.collection('posts').add({
+            displayName:'Mayowa',
+            handle: "mayowa",
+            image: tweetImage,
+            text: tweet,
+            verified: true,
+            avatar:'https://images.unsplash.com/photo-1521714161819-15534968fc5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'
+
+        })
+
+        setTweet("");
+        setTweetImage("");
+
+    }
+
+
+
+
     return (
         <div className="tweetBox">
 
             <form>
                 <div className="tweetBox__input">
                     <Avatar src={avatar} className="tweetBox__avatar" />
-                    <input type='text' placeholder="What's happening?" />
+                    <input type='text' placeholder="What's happening?" value={tweet} onChange={e => setTweet(e.target.value)} />
                 </div>
                 <div className="tweetBox__icons">
-                    <ImageIcon />
+                    <ImageIcon onClick={e => setTweetImage(e.target.value)} />
                     <GifIcon />
                     <BarChartIcon />
                     <EmojiEmotionsIcon />
                     <EventIcon />
-                    <Button className="tweetBox__btn">Tweet</Button>
+                    <Button type='submit' className="tweetBox__btn" onClick={handleSubmit}>Tweet</Button>
 
                 </div>
             </form>
